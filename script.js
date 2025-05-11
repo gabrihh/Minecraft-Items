1dfd936191e2a14ebb3c4762cd93be917caba29d
GabrihhDEV_ <120270743+gabrihh@users.noreply.github.com>
2025-05-10 22:02:45 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+
+async function fetchItems() {
+    try {
+        const itemsResponse = await fetch('https://raw.githubusercontent.com/PetarMc1/Minecraft-Items-API/refs/heads/main/api/Data/1.11/items.json');
+        const itemsData = await itemsResponse.json();
+        const iconsResponse = await fetch('https://minecraft-api.vercel.app/api/items');
+        const iconsData = await iconsResponse.json();
+        displayItems(itemsData, iconsData);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+function displayItems(itemsData, iconsData) {
+    const itemListContainer = document.getElementById('item-list');
+    itemListContainer.innerHTML = '';
+    itemsData.forEach(item => {
+        const icon = iconsData.find(icon => icon.namespacedId === item.name);
+        if (icon) {
+            const itemElement = document.createElement('div');
+            itemElement.classList.add('item');
+            itemElement.innerHTML = `
+                <img src="${icon.image}" alt="${item.displayName}">
+                <p><strong>${item.displayName}</strong></p>
+                <p>ID: ${item.id}</p>
+                <p>${icon.description}</p>
+            `;
+            itemListContainer.appendChild(itemElement);
+        }
+    });
+}
+
+fetchItems();
